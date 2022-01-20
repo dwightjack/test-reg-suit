62a3f79b7d3f443845eb0a8bffd7e776add39a1b
dwightjack <marco.solazzi@gmail.com>
2022-01-20 15:08:04 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Button from "./components/Button.vue";
+
+function sayHello() {
+  alert("hello!");
+}
+</script>
+
+<template>
+  <Button label="Hello World" @click="sayHello" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,88 @@
+<template>
+  <button type="button" :class="classes" @click="onClick" :style="style">
+    {{ label }}
+  </button>
+</template>
+
+<script lang="ts">
+import "./button.css";
+import { computed, defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    primary: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String,
+      validator: function (value: string) {
+        return ["small", "medium", "large"].indexOf(value) !== -1;
+      },
+    },
+    backgroundColor: {
+      type: String,
+      default: "",
+    },
+  },
+
+  emits: ["click"],
+
+  setup(props, { emit }) {
+    const classes = computed(() => ({
+      "storybook-button": true,
+      "storybook-button--primary": props.primary,
+      "storybook-button--secondary": !props.primary,
+      [`storybook-button--${props.size || "medium"}`]: true,
+    }));
+
+    const style = computed(() => ({
+      backgroundColor: props.backgroundColor,
+    }));
+
+    return {
+      classes,
+      style,
+      onClick() {
+        emit("click");
+      },
+    };
+  },
+});
+</script>
+<style scoped>
+.storybook-button {
+  font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-weight: 700;
+  border: 0;
+  border-radius: 3em;
+  cursor: pointer;
+  display: inline-block;
+  line-height: 1;
+}
+.storybook-button--primary {
+  color: white;
+  background-color: #1ea7fd;
+}
+.storybook-button--secondary {
+  color: #333;
+  background-color: transparent;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+}
+.storybook-button--small {
+  font-size: 16px;
+  padding: 10px 26px;
+}
+.storybook-button--medium {
+  font-size: 14px;
+  padding: 11px 20px;
+}
+.storybook-button--large {
+  font-size: 16px;
+  padding: 12px 24px;
+}
+</style>
