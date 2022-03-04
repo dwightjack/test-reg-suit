c2d69a00f9af4ba84bd852ccfa54d4b1c0d70e21
dwightjack <marco.solazzi@gmail.com>
2022-03-04 18:05:33 +0900

    test bash script

---
+++ scripts/regsuit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PACKAGES="${@:-"core layout"}"
+for PACKAGE in $PACKAGES
+do
+  echo "Processing $PACKAGE"
+  echo "- Building Storybook..."
+  npx --no-install build-storybook -c ./packages/$PACKAGE/.storybook -o ./packages/$PACKAGE/dist-storybook
+  echo "- Grab screenshots..." 
+  npx --no-install storycap --serverCmd "http-server ./packages/$PACKAGE/dist-storybook -s -p 9020" --outDir ./packages/$PACKAGE/__screenshots__ http://localhost:9020
+  cd ./packages/$PACKAGE
+  node ../../node_modules/.bin/reg-suit run -c ./regconfig.json
+  cd -
+done
