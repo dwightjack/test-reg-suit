49dc58a315e717152e94451c1cb18bf9d6a18329
dwightjack <marco.solazzi@gmail.com>
2022-03-04 18:12:41 +0900

    fix exit

--- scripts/regsuit.sh
+++ scripts/regsuit.sh
@@ -4,10 +4,10 @@ for PACKAGE in $PACKAGES
 do
   echo "Processing $PACKAGE"
   echo "- Building Storybook..."
-  npx --no-install build-storybook -c ./packages/$PACKAGE/.storybook -o ./packages/$PACKAGE/dist-storybook
+  npx --no-install build-storybook -c ./packages/$PACKAGE/.storybook -o ./packages/$PACKAGE/dist-storybook || exit 1; 
   echo "- Grab screenshots..." 
-  npx --no-install storycap --serverCmd "http-server ./packages/$PACKAGE/dist-storybook -s -p 9020" --outDir ./packages/$PACKAGE/__screenshots__ http://localhost:9020
+  npx --no-install storycap --serverCmd "http-server ./packages/$PACKAGE/dist-storybook -s -p 9020" --outDir ./packages/$PACKAGE/__screenshots__ http://localhost:9020  || exit 1; 
   cd ./packages/$PACKAGE
-  node ../../node_modules/.bin/reg-suit run -c ./regconfig.json
+  node ../../node_modules/.bin/reg-suit run -c ./regconfig.json 
   cd -
 done
