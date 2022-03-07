e104b86bc7a9f9d0203afc0705a58c7cd8e7aba1
dwightjack <marco.solazzi@gmail.com>
2022-03-07 10:48:11 +0900

    fix

--- scripts/regsuit.sh
+++ scripts/regsuit.sh
@@ -4,7 +4,7 @@ for PACKAGE in $PACKAGES
 do
   echo "Processing $PACKAGE"
   echo "- Building Storybook..."
-  npx --no-install build-storybook -c ./packages/$PACKAGE/.storybook -o ./packages/$PACKAGE/dist-storybook || exit 1; 
+  npx --no-install build-storybook -c ./packages/$PACKAGE/.storybook -o ./packages/$PACKAGE/dist-storybook --quiet || exit 1; 
   echo "- Grab screenshots..." 
   npx --no-install storycap --serverCmd "http-server ./packages/$PACKAGE/dist-storybook -s -p 9020" --outDir ./packages/$PACKAGE/__screenshots__ http://localhost:9020  || exit 1; 
   cd ./packages/$PACKAGE
